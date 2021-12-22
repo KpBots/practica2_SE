73df5011e02c49d7c30bb653c76fd37cae4f8a7b
Alberto Ferreiro Campello <a.fcampello@udc.es>
2021-12-22 18:07:14 +0100

    Added timed reset

--- main.c
+++ main.c
@@ -6,13 +6,17 @@
 #define SW1_POS (3)
 #define SW2_POS (12)
 
-int CORE_CLOCK = 48000000;
-int MS = 0;
+const int CORE_CLOCK = 48000000;
+const int DURATION_SHORT = 1000000;
+const int DURATION_LONG = DURATION_SHORT * 3;
+const int WAITING = 0;
+const int READING = 1;
 int STATUS_SW1 = 0;
 int STATUS_SW2 = 0;
 int SYSTEM_STATUS = 0;
 int DOTS = 0;
 int DASHES = 0;
+
 
 // LED (RG)
 // LED_GREEN = PTD5 (pin 98)
@@ -30,11 +34,15 @@ void irclk_ini()
   MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
 }
 
-void delay(void)
+int delay(int duration, int waiting)
 {
   volatile int i;
 
-  for (i = 0; i < 1000000; i++);
+  for (i = 0; i < duration; i++){
+    if (!waiting && SYSTEM_STATUS)
+      return i;
+  }
+  return i;
 }
 
 
@@ -107,7 +115,7 @@ void PORTDIntHandler(void) {
   int pressed_switch = PORTC->ISFR;
   PORTC->ISFR = 0xFFFFFFFF; // Clear IRQ
 
-    // SW1
+  // SW1
   if(pressed_switch == (0x8)) {
     ++STATUS_SW1;
   }
@@ -121,48 +129,54 @@ void PORTDIntHandler(void) {
 }
 
 // Hit condition: (else, it is a miss)
-// - Left switch matches red light
-// - Right switch matches green light
+// - Left switch = Dash
+// - Right switch = Dot
 
 int main(void)
 {
   led_green_init();
   led_red_init();
-  sw1_init();
-  sw2_init();
-  irclk_ini(); // Enable internal ref clk to use by LCD
-
-  lcd_ini();
-  lcd_display_time(00, 00);
-
   led_green_off();
   led_red_off();
 
+  sw1_init();
+  sw2_init();
+
+  irclk_ini(); // Enable internal ref clk to use by LCD
+  lcd_ini();
+
   volatile unsigned int result = 0;
+  volatile int elapsed_time = 0;
   while (result != 505) {
-    // Bloqued until any button are pushed
-    while(!SYSTEM_STATUS);
+
+    // Bloqued until any button are pushed unless it waits too long
+    while(!SYSTEM_STATUS) {
+      if(elapsed_time)
+        break;
+    }
+
     // Update
     if(STATUS_SW1) {
       led_green_on();
-      delay();
+      delay(DURATION_SHORT, READING);
       led_green_off();
       ++DOTS;
     } else if(STATUS_SW2) {
       led_green_on();
-      delay();
-      delay();
-      delay();
+      delay(DURATION_LONG, READING);
       led_green_off();
       ++DASHES;
     }
 
     // S
     if (DOTS == 3 && DASHES == 0) {
+      // First S of the sequence
       if(!result) {
         result += 5;
+      // Last S of the sequence
       } else if(result == 50) {
         result *= 10.1;
+      // If the sequence is broken
       } else {
         result = 0;
       }
@@ -173,12 +187,14 @@ int main(void)
     if (DASHES == 3 && DOTS == 0) {
       if(result == 5) {
         result *= 10;
+      // If the sequence is broken
       } else {
         result = 0;
       }
       DASHES = 0;
     }
 
+    // Invalid character
     if (DOTS > 5 || DASHES > 5){
       DOTS = 0;
       DASHES = 0;
@@ -191,12 +207,25 @@ int main(void)
     STATUS_SW1 = 0;
     STATUS_SW2 = 0;
     SYSTEM_STATUS = 0;
+
+    elapsed_time = delay(DURATION_LONG * 2, WAITING);
+
+    // Waiting too long
+    if (elapsed_time > DURATION_LONG){
+      DOTS = 0;
+      DASHES = 0;
+      result = 0;
+    }
+    else {
+      elapsed_time = 0;
+    }
   }
 
-  // Stop game and show blinking final result in LCD: hits:misses
+  // Stop reading the input and show blinking SOS in the LCD
   while (1) {
     LCD->AR |= LCD_AR_BLINK(1);
     LCD->AR |= LCD_AR_BRATE(0xBB);
+    led_red_on();
     lcd_display_n_dec(result);
   }
 
