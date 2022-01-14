1edc4a77e29593a07efadf26d31991e830de47ed
Alberto Ferreiro Campello <a.fcampello@udc.es>
2022-01-14 06:25:52 +0100

    Traballo Tutelado

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include "MKL46Z4.h"
-#include "lcd.h"
 
 #define LED_GREEN_POS (5)
 #define LED_RED_POS (29)
 #define SW1_POS (3)
 #define SW2_POS (12)
+#define RIGHT_MOTOR_FRONT (5)
+#define RIGHT_MOTOR_BACK (4)
+#define LEFT_MOTOR_FRONT (8)
+#define LEFT_MOTOR_BACK (9)
+#define RIGHT_IR (3)
+#define LEFT_IR (12)
 
 const int CORE_CLOCK = 48000000;
-const int DURATION_SHORT = 1000000;
-const int DURATION_LONG = DURATION_SHORT * 3;
-const int WAITING = 0;
-const int READING = 1;
+
 int STATUS_SW1 = 0;
 int STATUS_SW2 = 0;
-int SYSTEM_STATUS = 0;
-int DOTS = 0;
-int DASHES = 0;
+int BUTTONS_STATUS = 0;
 
 
 // LED (RG)
@@ -26,6 +26,18 @@ int DASHES = 0;
 // RIGHT (SW1) = PTC3 (pin 73)
 // LEFT (SW2) = PTC12 (pin 88)
 
+/*** The following pins are named following the classic layout of an Arduino UNO ***/
+// MOTORS
+// RIGHT_MOTOR_FRONT = PTA4 (Digital pin 4)
+// RIGHT_MOTOR_BACK = PTA5 (Digital pin 5)
+// LEFT_MOTOR_FRONT = PTC8 (Digital pin 6)
+// LEFT_MOTOR_BACK = PTC9 (Digital pin 7)
+
+// IR SENSORS
+// RIGHT_IR = PTD3 (Digital pin 2)
+// LEFT_IR = PTA12 (Digital pin 3)
+
+
 // Enable IRCLK (Internal Reference Clock)
 // see Chapter 24 in MCU doc
 void irclk_ini()
@@ -34,14 +46,11 @@ void irclk_ini()
   MCG->C2 = MCG_C2_IRCS(0); //0 32KHZ internal reference clock; 1= 4MHz irc
 }
 
-int delay(int duration, int waiting)
+int delay(int duration)
 {
   volatile int i;
 
-  for (i = 0; i < duration; i++){
-    if (!waiting && SYSTEM_STATUS)
-      return i;
-  }
+  for (i = 0; i < duration; i++){}
   return i;
 }
 
@@ -82,6 +91,54 @@ void led_red_off(void) {
 }
 
 
+// Motors
+void right_motor_init()
+{
+  SIM->COPC = 0;                            // Desactiva el Watchdog
+  SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;       // Conecta el reloj al puerto A
+  PORTA->PCR[RIGHT_MOTOR_FRONT] = PORT_PCR_MUX(1); // Configura los pines necesarios como GPIO
+  PORTA->PCR[RIGHT_MOTOR_BACK] = PORT_PCR_MUX(1);  // Configura los pines necesarios como GPIO
+  GPIOA->PDDR |= (1 << RIGHT_MOTOR_FRONT);  // Se configura como pin de salida
+  GPIOA->PSOR |= (1 << RIGHT_MOTOR_FRONT);  // Se pone a 1 el pin de salida
+  GPIOA->PDDR |= (1 << RIGHT_MOTOR_BACK);   // Se configura como pin de salida
+  GPIOA->PSOR |= (1 << RIGHT_MOTOR_BACK);   // Se pone a 1 el pin de salida
+}
+
+void right_motor_on(void)
+{
+  GPIOA->PCOR |= (1 << RIGHT_MOTOR_FRONT);
+  GPIOA->PSOR |= (1 << RIGHT_MOTOR_BACK);
+}
+
+void right_motor_off(void) {
+  GPIOA->PSOR |= (1 << RIGHT_MOTOR_FRONT);
+  GPIOA->PSOR |= (1 << RIGHT_MOTOR_BACK);
+}
+
+void left_motor_init()
+{
+  SIM->COPC = 0;                            // Desactiva el Watchdog
+  SIM->SCGC5 |= SIM_SCGC5_PORTC_MASK;       // Conecta el reloj al puerto A
+  PORTC->PCR[LEFT_MOTOR_FRONT] = PORT_PCR_MUX(1); // Configura los pines necesarios como GPIO
+  PORTC->PCR[LEFT_MOTOR_BACK] = PORT_PCR_MUX(1);  // Configura los pines necesarios como GPIO
+  GPIOC->PDDR |= (1 << LEFT_MOTOR_FRONT);  // Se configura como pin de salida
+  GPIOC->PSOR |= (1 << LEFT_MOTOR_FRONT);  // Se pone a 1 el pin de salida
+  GPIOC->PDDR |= (1 << LEFT_MOTOR_BACK);   // Se configura como pin de salida
+  GPIOC->PSOR |= (1 << LEFT_MOTOR_BACK);   // Se pone a 1 el pin de salida
+}
+
+void left_motor_on(void)
+{
+  GPIOC->PCOR |= (1 << LEFT_MOTOR_FRONT);
+  GPIOC->PSOR |= (1 << LEFT_MOTOR_BACK);
+}
+
+void left_motor_off(void) {
+  GPIOC->PSOR |= (1 << LEFT_MOTOR_FRONT);
+  GPIOC->PSOR |= (1 << LEFT_MOTOR_BACK);
+}
+
+
 // Switches
 void sw1_init() {
   SIM->COPC = 0;              // Desactiva el Watchdog
@@ -111,9 +168,11 @@ void sw2_init() {
   NVIC_EnableIRQ(31);     // Activa la interrupcion
 }
 
+// Switches IRQs
 void PORTDIntHandler(void) {
+  // Buttons
   int pressed_switch = PORTC->ISFR;
-  PORTC->ISFR = 0xFFFFFFFF; // Clear IRQ
+  PORTC->ISFR = 0xFFFFFFFF;
 
   // SW1
   if(pressed_switch == (0x8)) {
@@ -125,13 +184,46 @@ void PORTDIntHandler(void) {
     ++STATUS_SW2;
   }
 
-  SYSTEM_STATUS = STATUS_SW1 + STATUS_SW2;
+  BUTTONS_STATUS = STATUS_SW1 + STATUS_SW2;
 }
 
-// Hit condition: (else, it is a miss)
-// - Left switch = Dash
-// - Right switch = Dot
 
+// IR SENSORS
+// RIGHT_IR = PTD3 (Digital pin 2)
+// LEFT_IR = PTA12 (Digital pin 3)
+void right_ir_init() {
+  SIM->COPC = 0;                        // Desactiva el Watchdog
+  SIM->SCGC5 |= SIM_SCGC5_PORTD_MASK;   // Conecta el reloj al puerto D
+
+  PORTD->PCR[RIGHT_IR] |= PORT_PCR_MUX(1); // Activa el GPIO
+  PORTD->PCR[RIGHT_IR] |= PORT_PCR_PE_MASK;// Pull enable, habilita la resistencia interna
+  PORTD->PCR[RIGHT_IR] |= PORT_PCR_PS_MASK;// Pull select, selecciona el modo de funcionamiento pullup/pulldown
+  GPIOD->PDDR &= ~(1 << RIGHT_IR);
+}
+
+void left_ir_init() {
+  SIM->COPC = 0;              // Desactiva el Watchdog
+  SIM->SCGC5 |= SIM_SCGC5_PORTA_MASK;   // Conecta el reloj al puerto A
+
+  PORTA->PCR[LEFT_IR] |= PORT_PCR_MUX(1); // Activa el GPIO
+  PORTA->PCR[LEFT_IR] |= PORT_PCR_PE_MASK;// Pull enable, habilita la resistencia interna
+  PORTA->PCR[LEFT_IR] |= PORT_PCR_PS_MASK;// Pull select, selecciona el modo de funcionamiento pullup/pulldown
+  GPIOA->PDDR &= ~(1 << LEFT_IR);
+}
+
+int right_ir_active() {
+  return( !(GPIOD->PDIR & (1 << RIGHT_IR)) );
+}
+
+int left_ir_active() {
+  return( !(GPIOA->PDIR & (1 << LEFT_IR)) );
+}
+
+
+/* Main code */
+
+// 1- Sleep until any switch is pushed (SW1 or SW3)
+// 2- Line follow
 int main(void)
 {
   led_green_init();
@@ -142,94 +234,36 @@ int main(void)
   sw1_init();
   sw2_init();
 
-  irclk_ini(); // Enable internal ref clk to use by LCD
-  lcd_ini();
+  right_motor_init();
+  left_motor_init();
+  right_motor_off();
+  left_motor_off();
 
-  volatile unsigned int result = 0;
-  volatile int elapsed_time = 0;
-  while (result != 505) {
+  right_ir_init();
+  left_ir_init();
 
-    // Bloqued until any button are pushed unless it waits too long
-    while(!SYSTEM_STATUS) {
-      if(elapsed_time)
-        break;
-    }
+  // The microcontroller awaits in sleep state
+  asm("wfi");
 
-    // Update
-    if(STATUS_SW1) {
+  // Line follow algorythm
+  while(1){
+    if (right_ir_active())
+    {
+      left_motor_on();
       led_green_on();
-      delay(DURATION_SHORT, READING);
+    } else {
+      left_motor_off();
       led_green_off();
-      ++DOTS;
-    } else if(STATUS_SW2) {
-      led_green_on();
-      delay(DURATION_LONG, READING);
-      led_green_off();
-      ++DASHES;
     }
 
-    // S
-    if (DOTS == 3 && DASHES == 0) {
-      // First S of the sequence
-      if(!result) {
-        result += 5;
-      // Last S of the sequence
-      } else if(result == 50) {
-        result *= 10.1;
-      // If the sequence is broken
-      } else {
-        result = 0;
-      }
-      DOTS = 0;
+    if (left_ir_active())
+    {
+      right_motor_on();
+      led_red_on();
+    } else {
+      right_motor_off();
+      led_red_off();
     }
-
-    // O
-    if (DASHES == 3 && DOTS == 0) {
-      if(result == 5) {
-        result *= 10;
-      // If the sequence is broken
-      } else {
-        result = 0;
-      }
-      DASHES = 0;
-    }
-
-    // Invalid character
-    if (DOTS > 5 || DASHES > 5){
-      DOTS = 0;
-      DASHES = 0;
-      result = 0;
-    }
-
-    // Update LCD
-    lcd_display_n_dec(result);
-    // Update system values
-    STATUS_SW1 = 0;
-    STATUS_SW2 = 0;
-    SYSTEM_STATUS = 0;
-
-    if(result == 505)
-      break;
-
-    elapsed_time = delay(DURATION_LONG * 2, WAITING);
-
-    // Waiting too long
-    if (elapsed_time > DURATION_LONG){
-      DOTS = 0;
-      DASHES = 0;
-      result = 0;
-    }
-    else {
-      elapsed_time = 0;
-    }
-  }
-
-  // Stop reading the input and show blinking SOS in the LCD
-  while (1) {
-    LCD->AR |= LCD_AR_BLINK(1);
-    LCD->AR |= LCD_AR_BRATE(0xBB);
-    led_red_on();
-    lcd_display_n_dec(result);
   }
 
   return 0;
